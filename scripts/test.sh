090f5a2ca5cc86eef32c0ce69a426a1968d404b7
Frank-wsl <2961429602@qq.com>
2023-05-09 23:21:20 +0800

    first commit

---
+++ scripts/test.sh
@@ -0,0 +1,6 @@
+python predict.py -i test/00001.png -o output/output01.png --model checkpoints/checkpoint_epoch25.pth
+python predict.py -i test/00002.png -o output/output02.png --model checkpoints/checkpoint_epoch25.pth
+python predict.py -i test/00003.png -o output/output03.png --model checkpoints/checkpoint_epoch25.pth
+python predict.py -i test/00007.png -o output/output07.png --model checkpoints/checkpoint_epoch25.pth
+python predict.py -i test/00019.png -o output/output19.png --model checkpoints/checkpoint_epoch25.pth
+python predict.py -i test/00038.png -o output/output38.png --model checkpoints/checkpoint_epoch25.pth
